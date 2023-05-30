de4a27c87bf80552fff75a52c3f73d651ae0ad80
Juan Pedro <87458914+juampibastias@users.noreply.github.com>
2023-05-30 20:12:24 -0300

    Update PaymentController.js

--- Controllers/PaymentController.js
+++ Controllers/PaymentController.js
@@ -1,21 +1,24 @@
 class PaymentController {
-  
-    constructor(subscriptionService) {
-      this.subscriptionService = subscriptionService;
-    }
-    async getPaymentLink(req, res) {
 
-      try {
-        const payment = await this.subscriptionService.createPayment(req, res);
-      
-        return res.json(payment);
-      } catch (error) {
-        console.log(error);
-        return res
-          .status(500)
-          .json({ error: true, msg: "Failed to create payment" });
-      } 
+  constructor(subscriptionService) {
+    this.subscriptionService = subscriptionService;
+  }
+  async getPaymentLink(req, res) {
+
+    try {
+      const payment = await this.subscriptionService.createPayment(req, res);
+      return payment.init_point;
+    } catch (error) {
+      console.log(error);
+      return res
+        .status(500)
+        .json({
+          error: true,
+          msg: "Failed to create payment"
+        });
     }
+
+  }
 }
-  
-  module.exports = PaymentController;
+
+module.exports = PaymentController;
