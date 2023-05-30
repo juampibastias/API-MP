1558e564c906eab0350c23be366555ffe3b9eea4
Juan Pedro <87458914+juampibastias@users.noreply.github.com>
2023-05-30 20:10:31 -0300

    Update index.js

--- routes/index.js
+++ routes/index.js
@@ -8,18 +8,20 @@ const PaymentInstance = new PaymentController(new PaymentService());
 
 
 /* GET home page. */
-router.get('/', function(req, res, next) {
+router.get('/', function (req, res, next) {
   return res.json({
-    "/payment": "generates a payment link" 
+    "/payment": "generates a payment link"
   })
 
 });
-router.get('/payment', function(req, res, next) {
+router.get('/payment', function (req, res, next) {
   PaymentInstance.getPaymentLink(req, res)
- });
+});
 
-router.post('/payment', async function(req, res, next) {
-  PaymentInstance.getPaymentLink(req.body.data, res)
- });
+router.post('/payment', async function (req, res, next) {
+  res.json({
+    data: await PaymentInstance.getPaymentLink(req.body.data)
+  })
+});
 
 module.exports = router;
